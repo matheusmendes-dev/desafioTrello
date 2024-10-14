0974c72044355cb6480567fc36fc4abb0dd447f2
Matheus Mendes <matheusmendess90@gmail.com>
2024-10-14 00:12:50 -0300

    build: added retrofit dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.jetbrainsKotlinKapt) apply false
-    alias(libs.plugins.daggerHilt) apply false
+    alias(libs.plugins.googleDaggerHilt) apply false
 }
