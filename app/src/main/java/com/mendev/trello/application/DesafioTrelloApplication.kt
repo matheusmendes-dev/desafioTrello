b8e2882dcb18fc9e11deabf935c930b7b7c45783
Matheus Mendes <matheusmendess90@gmail.com>
2024-10-14 19:48:32 -0300

    build: dagger initial settings

---
+++ app/src/main/java/com/mendev/trello/application/DesafioTrelloApplication.kt
@@ -0,0 +1,7 @@
+package com.mendev.trello.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DesafioTrelloApplication : Application()
--- app/src/main/java/com/mendev/trello/ui/MainActivity.kt
+++ app/src/main/java/com/mendev/trello/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.mendev.trello
+package com.mendev.trello.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,13 +11,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.mendev.trello.ui.theme.DesafioTrelloTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DesafioTrelloTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
